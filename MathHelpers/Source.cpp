98280d7ac8f912b36eb4854ae7eda0ebfae97c08
Jonathan Suryadiputra <C00235450@itcarlow.ie>
2021-02-24 23:28:58 +0000

    Added menu pie in a loop

--- MathHelpers/Source.cpp
+++ MathHelpers/Source.cpp
@@ -18,7 +18,7 @@ int gcd(int a, int b) {
 	return a;
 }
 
-string inverse(int number, int mod) {
+bool inverse(int number, int mod, int &answer) {
 	if (gcd(number, mod) == 1) {
 		int x = 0;
 		int remainder = 0;
@@ -26,13 +26,108 @@ string inverse(int number, int mod) {
 			x++;
 			remainder = (number * x) % mod;
 		}
-		return to_string(x);
+		answer = x;
+		return true;
 	}
 	else {
-		return "no inverse found";
+		return false;
 	}
 }
 
+int eulerphi(int number) {
+	int count = 0;
+	for (int i = 1; i <= number; i++) {
+		if (gcd(i, number) == 1) {
+			count++;
+		}
+	}
+	return count;
+}
+
+// menu function
+void menu() {
+	cout << "Enter the corresponding number to select an option :";
+	cout << "\n\n------------Menu------------";
+	cout << "\n\n1.   GCD (Pair)";
+	cout << "\n2.   Inverse of Congruence";
+	cout << "\n3.   Euler Phi";
+	cout << "\n\n4.   Exit\n\n";
+}
+
 int main() {
-	cout << inverse(3, 5);
+	int choice;
+	bool exit = false;
+	int numberIn;
+	int numberIn2;
+	int inv;
+
+    while (exit == false) {
+        system("cls"); // clear screen
+        menu(); // print out the menu
+        cout << "Enter your option here : "; // prompt user for the choice number
+        cin >> choice; // fish the choice number into a variable
+        cout << endl;
+        // input validation in case the user enters a non-integer
+        if (cin.fail()) {
+            cout << "Invalid option, try again." << endl;
+            cin.clear();
+            cin.ignore(1000, '\n');
+            cout << endl;
+            system("pause");
+        }
+        else {
+            switch (choice) {
+            case 1:
+				cout << "Please enter the first number:" << endl;
+				cin >> numberIn;
+				cout << endl;
+				cout << "Please enter the second number:" << endl;
+				cin >> numberIn2;
+				cout << endl;
+				cout << "The GCD of the two numbers entered are:" << endl;
+				cout << gcd(numberIn, numberIn2) << endl;
+				cout << endl;
+				system("pause"); // system pause
+                break;
+			case 2:
+				cout << "Please enter the first number:" << endl;
+				cin >> numberIn;
+				cout << endl;
+				cout << "Please enter the second number:" << endl;
+				cin >> numberIn2;
+				cout << endl;
+				if (inverse(numberIn, numberIn2, inv) == true) {
+					cout << "The inverse of the congruence equation is:" << endl;
+					cout << inv << endl;
+				}
+				else {
+					cout << "No inverse found!" << endl;
+				}
+				cout << endl;
+				system("pause"); // system pause
+				break;
+			case 3:
+				cout << "Please enter a number:" << endl;
+				cin >> numberIn;
+				cout << endl;
+				cout << "The Euler Phi of the number is:" << endl;
+				cout << eulerphi(numberIn) << endl;
+				cout << endl;
+				system("pause"); // system pause
+				break;
+			case 4: // exit
+				cout << "Good Bye!" << endl << endl;
+				exit = true;
+				break;
+			default: // input validation in case choice option is < 1 or > 8
+				cout << "Invalid option, try again." << endl;
+				cout << endl;
+				system("pause");
+				break;
+			}
+        }
+    }
+	cout << "Press any key to close this window . . ." << endl;
+	system("pause>nul");
+	return 0;
 }
